cbffb6d1d2da07cab9c562146adcf347ab0b5e97
Yun5141 <yun.fang.17@ucl.ac.uk>
2020-03-13 23:06:09 +0000

    a

---
+++ devel/share/gennodejs/ros/stdr_msgs/msg/_index.js
@@ -0,0 +1,96 @@
+
+"use strict";
+
+let Noise = require('./Noise.js');
+let CO2SensorMsg = require('./CO2SensorMsg.js');
+let RfidSensorMsg = require('./RfidSensorMsg.js');
+let SoundSourceVector = require('./SoundSourceVector.js');
+let SoundSensorMeasurementMsg = require('./SoundSensorMeasurementMsg.js');
+let ThermalSensorMeasurementMsg = require('./ThermalSensorMeasurementMsg.js');
+let SoundSource = require('./SoundSource.js');
+let RfidTagVector = require('./RfidTagVector.js');
+let RobotMsg = require('./RobotMsg.js');
+let ThermalSensorMsg = require('./ThermalSensorMsg.js');
+let ThermalSource = require('./ThermalSource.js');
+let RfidTag = require('./RfidTag.js');
+let SoundSensorMsg = require('./SoundSensorMsg.js');
+let CO2SourceVector = require('./CO2SourceVector.js');
+let RobotIndexedMsg = require('./RobotIndexedMsg.js');
+let LaserSensorMsg = require('./LaserSensorMsg.js');
+let SonarSensorMsg = require('./SonarSensorMsg.js');
+let KinematicMsg = require('./KinematicMsg.js');
+let ThermalSourceVector = require('./ThermalSourceVector.js');
+let CO2SensorMeasurementMsg = require('./CO2SensorMeasurementMsg.js');
+let FootprintMsg = require('./FootprintMsg.js');
+let CO2Source = require('./CO2Source.js');
+let RobotIndexedVectorMsg = require('./RobotIndexedVectorMsg.js');
+let RfidSensorMeasurementMsg = require('./RfidSensorMeasurementMsg.js');
+let RegisterRobotAction = require('./RegisterRobotAction.js');
+let DeleteRobotFeedback = require('./DeleteRobotFeedback.js');
+let RegisterRobotActionGoal = require('./RegisterRobotActionGoal.js');
+let RegisterRobotResult = require('./RegisterRobotResult.js');
+let RegisterRobotFeedback = require('./RegisterRobotFeedback.js');
+let DeleteRobotActionFeedback = require('./DeleteRobotActionFeedback.js');
+let SpawnRobotFeedback = require('./SpawnRobotFeedback.js');
+let SpawnRobotGoal = require('./SpawnRobotGoal.js');
+let RegisterRobotGoal = require('./RegisterRobotGoal.js');
+let DeleteRobotGoal = require('./DeleteRobotGoal.js');
+let RegisterRobotActionFeedback = require('./RegisterRobotActionFeedback.js');
+let DeleteRobotActionGoal = require('./DeleteRobotActionGoal.js');
+let SpawnRobotActionResult = require('./SpawnRobotActionResult.js');
+let SpawnRobotActionGoal = require('./SpawnRobotActionGoal.js');
+let SpawnRobotActionFeedback = require('./SpawnRobotActionFeedback.js');
+let DeleteRobotActionResult = require('./DeleteRobotActionResult.js');
+let SpawnRobotResult = require('./SpawnRobotResult.js');
+let RegisterRobotActionResult = require('./RegisterRobotActionResult.js');
+let SpawnRobotAction = require('./SpawnRobotAction.js');
+let DeleteRobotAction = require('./DeleteRobotAction.js');
+let DeleteRobotResult = require('./DeleteRobotResult.js');
+
+module.exports = {
+  Noise: Noise,
+  CO2SensorMsg: CO2SensorMsg,
+  RfidSensorMsg: RfidSensorMsg,
+  SoundSourceVector: SoundSourceVector,
+  SoundSensorMeasurementMsg: SoundSensorMeasurementMsg,
+  ThermalSensorMeasurementMsg: ThermalSensorMeasurementMsg,
+  SoundSource: SoundSource,
+  RfidTagVector: RfidTagVector,
+  RobotMsg: RobotMsg,
+  ThermalSensorMsg: ThermalSensorMsg,
+  ThermalSource: ThermalSource,
+  RfidTag: RfidTag,
+  SoundSensorMsg: SoundSensorMsg,
+  CO2SourceVector: CO2SourceVector,
+  RobotIndexedMsg: RobotIndexedMsg,
+  LaserSensorMsg: LaserSensorMsg,
+  SonarSensorMsg: SonarSensorMsg,
+  KinematicMsg: KinematicMsg,
+  ThermalSourceVector: ThermalSourceVector,
+  CO2SensorMeasurementMsg: CO2SensorMeasurementMsg,
+  FootprintMsg: FootprintMsg,
+  CO2Source: CO2Source,
+  RobotIndexedVectorMsg: RobotIndexedVectorMsg,
+  RfidSensorMeasurementMsg: RfidSensorMeasurementMsg,
+  RegisterRobotAction: RegisterRobotAction,
+  DeleteRobotFeedback: DeleteRobotFeedback,
+  RegisterRobotActionGoal: RegisterRobotActionGoal,
+  RegisterRobotResult: RegisterRobotResult,
+  RegisterRobotFeedback: RegisterRobotFeedback,
+  DeleteRobotActionFeedback: DeleteRobotActionFeedback,
+  SpawnRobotFeedback: SpawnRobotFeedback,
+  SpawnRobotGoal: SpawnRobotGoal,
+  RegisterRobotGoal: RegisterRobotGoal,
+  DeleteRobotGoal: DeleteRobotGoal,
+  RegisterRobotActionFeedback: RegisterRobotActionFeedback,
+  DeleteRobotActionGoal: DeleteRobotActionGoal,
+  SpawnRobotActionResult: SpawnRobotActionResult,
+  SpawnRobotActionGoal: SpawnRobotActionGoal,
+  SpawnRobotActionFeedback: SpawnRobotActionFeedback,
+  DeleteRobotActionResult: DeleteRobotActionResult,
+  SpawnRobotResult: SpawnRobotResult,
+  RegisterRobotActionResult: RegisterRobotActionResult,
+  SpawnRobotAction: SpawnRobotAction,
+  DeleteRobotAction: DeleteRobotAction,
+  DeleteRobotResult: DeleteRobotResult,
+};
---
+++ devel/share/gennodejs/ros/stdr_msgs/srv/_index.js
@@ -0,0 +1,30 @@
+
+"use strict";
+
+let AddSoundSource = require('./AddSoundSource.js')
+let DeleteRfidTag = require('./DeleteRfidTag.js')
+let LoadExternalMap = require('./LoadExternalMap.js')
+let AddThermalSource = require('./AddThermalSource.js')
+let AddRfidTag = require('./AddRfidTag.js')
+let MoveRobot = require('./MoveRobot.js')
+let DeleteThermalSource = require('./DeleteThermalSource.js')
+let DeleteCO2Source = require('./DeleteCO2Source.js')
+let LoadMap = require('./LoadMap.js')
+let DeleteSoundSource = require('./DeleteSoundSource.js')
+let RegisterGui = require('./RegisterGui.js')
+let AddCO2Source = require('./AddCO2Source.js')
+
+module.exports = {
+  AddSoundSource: AddSoundSource,
+  DeleteRfidTag: DeleteRfidTag,
+  LoadExternalMap: LoadExternalMap,
+  AddThermalSource: AddThermalSource,
+  AddRfidTag: AddRfidTag,
+  MoveRobot: MoveRobot,
+  DeleteThermalSource: DeleteThermalSource,
+  DeleteCO2Source: DeleteCO2Source,
+  LoadMap: LoadMap,
+  DeleteSoundSource: DeleteSoundSource,
+  RegisterGui: RegisterGui,
+  AddCO2Source: AddCO2Source,
+};
